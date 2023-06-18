256616a1d96654e5bbc6bea41660a3f77147462f
Andrien Wiandyano <arnnied03@gmail.com>
2023-06-18 17:03:10 +0700

    remove index.php from url and add basic login & logout

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -10,15 +10,39 @@ class Auth extends BaseController
     {
         if ($this->request->is('get')) {
             // tampilkan halaman login
+            return view('auth/login');
         } else if ($this->request->is('post')) {
             // PROSES LOGIN
 
             // ambil data dari form (username, password)
+            $username = $this->request->getPost('username');
+            $password = $this->request->getPost('password');
 
             // validasi data dengan cara cek masing masing tabel (admin, teller, nasabah)
             // untuk menentukan apakah data tersebut ada di tabel tersebut
             // dan untuk menentukan role dari user tersebut
             // jika data ada di tabel admin, maka role = admin, dst
+            $role = "";
+            $user = "";
+
+
+            if ($u = $this->admin_model->where('username', $username)->first()) {
+                $user = $u;
+                $role = "admin";
+            } else if ($u = $this->teller_model->where('username', $username)->first()) {
+                $user = $u;
+                $role = "teller";
+            } else if ($u = $this->nasabah_model->where('username', $username)->first()) {
+                $user = $u;
+                $role = "nasabah";
+            }
+
+            $this->session->set('user', $user);
+            $this->session->set('role', $role);
+
+            var_dump($this->session->get('user'));
+            var_dump($this->session->get('role'));
+
 
             // jika data tidak ada di tabel manapun, maka tampilkan pesan error
             // menggunakan flashdata
@@ -34,6 +58,7 @@ class Auth extends BaseController
             // agar memudahkan pengecekan di halaman lain
 
             // redirect ke halaman sesuai role
+            return redirect('auth/login');
         } else {
             // jika bukan get atau post, maka tampilkan error 404
             throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
@@ -47,6 +72,6 @@ class Auth extends BaseController
         $this->session->setFlashdata('success', 'Anda berhasil logout');
 
         // redirect ke halaman login
-        return redirect()->to('/auth/login');
+        return redirect('auth/login');
     }
 }
